65147c8e5759197b8d5501898d637ac6ed70a703
Pierre-Ubuntu <pierre.perez@pucp.edu.pe>
2022-05-13 21:23:56 -0500

    comments added

--- face_recognition/train_model.py
+++ face_recognition/train_model.py
@@ -18,20 +18,17 @@ knownNames = []
 
 # loop over the image paths
 for (i, imagePath) in enumerate(imagePaths):
+	# Print how the training is going
+	print("[INFO] processing image {}/{}".format(i + 1,len(imagePaths))) #we process all images of all persons
 	# extract the person name from the image path
-	print("[INFO] processing image {}/{}".format(i + 1,
-		len(imagePaths)))
-	name = imagePath.split(os.path.sep)[-2]
+	name = imagePath.split(os.path.sep)[-2] 
 
-	# load the input image and convert it from RGB (OpenCV ordering)
-	# to dlib ordering (RGB)
+	# load the input image and convert it from RGB (OpenCV ordering) to dlib ordering (RGB)
 	image = cv2.imread(imagePath)
-	rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+	rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB) #Esto es importante, para poder entrenar a la red
 
-	# detect the (x, y)-coordinates of the bounding boxes
-	# corresponding to each face in the input image
-	boxes = face_recognition.face_locations(rgb,
-		model="hog")
+	# detect the (x, y)-coordinates of the bounding boxes corresponding to each face in the input image
+	boxes = face_recognition.face_locations(rgb,model="hog") #Se escoge el modelo hog
 
 	# compute the facial embedding for the face
 	encodings = face_recognition.face_encodings(rgb, boxes)
