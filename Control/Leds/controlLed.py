745effdc34196d2170f7001e5759ff6bbe76fc83
PierreSmartMirror <pierreramiro@gmail.com>
2022-06-03 15:57:09 -0500

    upd

--- Control/Leds/controlLed.py
+++ Control/Leds/controlLed.py
@@ -1,14 +1,15 @@
+#Importamos libreras
 import RPi.GPIO as gpio
 import time
 
 gpio.setmode(gpio.BOARD)
 gpio.setup(12, gpio.OUT)
 
-for  x in range ( 0, 10):
+for x in range (0, 10):
 
     gpio.output(12, True)
     time.sleep(0.5)
     gpio.output(12, False)
     time.sleep(0.5)
 
-print "Ejecución finalizada"
+print ("Ejecución finalizada")
