dce4a76a0ba9e06376b186ae40eb52a6e8f85088
PierreSmartMirror <pierreramiro@gmail.com>
2022-06-03 17:47:02 -0500

    half duplex protocol completed

--- WiFi/server.py
+++ WiFi/server.py
@@ -1,6 +1,6 @@
 import socket
 host = '10.100.108.56'
-port = 9999
+port = 9998
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.bind((host,port))
@@ -8,4 +8,8 @@ sock.listen(1)
 print(f"Server's ip: {host}\n Server's port: {port}")
 print(f"Waiting for incoming connections...")
 client, addr = sock.accept()
+data = client.recv(1024)
+data= data.decode()
+print(data)
 print(f"{addr} connected")
+client.close()
