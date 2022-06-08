aeba4abc23dfe7f8d162d434215fbe22477d7e16
PierreSmartMirror <pierreramiro@gmail.com>
2022-06-08 18:06:04 -0500

    upd

--- Bluetooth/pairing.sh
+++ Bluetooth/pairing.sh
@@ -1,5 +1,9 @@
+sudo /etc/init.d/bluetooth stop
 sudo /etc/init.d/bluetooth start #enciende el bluetooth
 #sudo /etc/init.d/bluetooth stop #apaga el bluetooth
 sudo hciconfig hci0 piscan #Enable inquiry scan and page scan of RPi's Bluetooth Server
 #sudo hciconfig -a hci0 | grep -i 'PSCAN *ISCAN' #Chequeamos si en realidad se hizo discoverable
+sudo systemctl enable bluetooth-autoconnect.service 
+sudo systemctl start bluetooth-autoconnect.service
+
 
