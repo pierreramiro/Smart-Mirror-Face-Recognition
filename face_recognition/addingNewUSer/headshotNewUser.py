44468b1117ce6a2b163ae9e984568c6739aa5156
Pierre_electronicaV <pierreramiro@gmail.com>
2022-06-08 17:46:14 -0500

    fotos headshot

---
+++ face_recognition/addingNewUSer/headshotNewUser.py
@@ -0,0 +1,5 @@
+import keyboard
+while True:
+    if keyboard.is_pressed("a"):
+        print("You pressed 'a'.")
+        break
--- face_recognition/headshots programs/headshots.py
+++ face_recognition/headshots programs/headshots.py
@@ -1,11 +1,8 @@
 import cv2
 
-name = 'Caroline' #replace with your name
+name = 'Pierre' #replace with your name
 
 cam = cv2.VideoCapture(0)
-
-cv2.namedWindow("press space to take a photo", cv2.WINDOW_NORMAL)
-cv2.resizeWindow("press space to take a photo", 500, 300)
 
 img_counter = 0
 
@@ -17,13 +14,14 @@ while True:
     cv2.imshow("press space to take a photo", frame)
 
     k = cv2.waitKey(1)
+    print(k)
     if k%256 == 27:
         # ESC pressed
         print("Escape hit, closing...")
         break
     elif k%256 == 32:
         # SPACE pressed
-        img_name = "dataset/"+ name +"/image_{}.jpg".format(img_counter)
+        img_name = "../dataset/"+ name +"/image_{}.jpg".format(img_counter)
         cv2.imwrite(img_name, frame)
         print("{} written!".format(img_name))
         img_counter += 1
