ed438eb3b857b542ccc201dbc97beb57ee9ffc89
Pierre-mirroll <pierreramiro@gmail.com>
2022-07-01 07:20:47 -0500

    upd

--- Control/controlRelays/controlRelays.py
+++ Control/controlRelays/controlRelays.py
@@ -4,8 +4,8 @@ import time
 
 gpio.setwarnings(False)
 CH1pin=10
-CH2pin=9     
-CH3pin=11
+CH2pin=11     
+CH3pin=9
 
 #Configuramos la convecion de pines
 gpio.setmode(gpio.BCM)
@@ -14,11 +14,9 @@ gpio.setup(CH1pin,gpio.OUT)
 gpio.setup(CH2pin,gpio.OUT)
 gpio.setup(CH3pin,gpio.OUT)
 #Configuramos todo en LOW
-gpio.output(CH1pin,gpio.LOW)
+gpio.output(CH1pin,gpio.HIGH)
 gpio.output(CH2pin,gpio.LOW)
 gpio.output(CH3pin,gpio.LOW)
 
 while True:
-    gpio.output(CH1pin,gpio.HIGH)
-    gpio.output(CH2pin,gpio.HIGH)
-    gpio.output(CH3pin,gpio.HIGH)
+    pass
