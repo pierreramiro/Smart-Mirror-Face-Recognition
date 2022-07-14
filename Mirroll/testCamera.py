94330dcee6327ad7b455e5399f3c03776aeb4f9a
PierreTumiPc2 <pierre.perez@pucp.edu.pe>
2022-07-14 10:58:14 -0500

    for testing camera

---
+++ Mirroll/testCamera.py
@@ -0,0 +1,15 @@
+import cv2
+cam = cv2.VideoCapture(0)
+while True:
+    ret, frame = cam.read()
+    if not ret:
+        print("failed to grab frame")
+        break
+    cv2.imshow("Camera Web", frame)
+    k = cv2.waitKey(1)
+    if k%256 == 27:
+        # ESC pressed
+        print("Escape hit, closing...")
+        break
+cam.release()
+cv2.destroyAllWindows()
