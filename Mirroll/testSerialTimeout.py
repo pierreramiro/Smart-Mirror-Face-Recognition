09aebe3365711ad96dc9328b750c64f19ae7b416
Pierre-Ubuntu <pierre.perez@pucp.edu.pe>
2022-07-17 18:09:51 -0500

    probamos el timeout

--- Mirroll/testSerialTimeout.py
+++ Mirroll/testSerialTimeout.py
@@ -1,8 +1,9 @@
 import serial #uart
 
-ser=serial.Serial ("/dev/ttyS0", 9600)
+ser=serial.Serial ("/dev/ttyS0", 9600,timeout=1)
+#ser=serial.Serial ("/dev/tty0", 9600,timeout=1)
 
 while True:
     received_data = ser.read()              #read serial port
-    print("sec")
+    print("longitud data:",len(received_data))
         
